fa14fe2443c395f2ed3a02f7c78f6753e757bc83
Aniket <aniketsinghparihar200702@gmail.com>
2025-02-27 19:15:57 +0530

    Update Snake Water Gun Game.py

--- Snake Water Gun Game.py
+++ Snake Water Gun Game.py
@@ -1,1 +1,28 @@
-
+import random
+computer=random.choice(['snake','water','gun'])
+print("Welcome to the Snake-Water-Gun Game")
+User=input("Enter your choice: ")
+Lwr=User.lower()
+def game(User,computer):
+    if Lwr==computer:
+        return "Tie"
+    elif Lwr=='snake':
+        if computer=='water':
+            return "User wins"
+        else:
+            return "Computer wins"
+    elif Lwr=='water':
+        if computer=='gun':
+            return "User wins"
+        else:
+            return "Computer wins"
+    elif Lwr=='gun':
+        if computer=='snake':
+            return "User wins"
+        else:
+            return "Computer wins"
+    else:
+        return "Invalid Input"
+print(f"Computer chose {computer}")
+print(f"You chose {User}")
+print(f"{game(User,computer)}")    
